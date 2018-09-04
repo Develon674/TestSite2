36eef546431d1d49180d80ecbea6cf0b80891649
William Roberts <william@buzzgraphicdesign.co.uk>
2018-09-04 08:55:07 +0100

    Changed description for master branch

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 /**
  * Plugin Name: Test Plugin
  * Author:      William Roberts
- * Description: This is my test plugin
+ * Description: This is my test plugin for the master branch
  * Version: 0.1.1
  */
