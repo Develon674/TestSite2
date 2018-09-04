d99e036ab0d9c62a3be5673755d4a694b6ea0161
William Roberts <william@buzzgraphicdesign.co.uk>
2018-09-04 15:30:59 +0100

    Add test plugin code

--- plugin.php
+++ plugin.php
@@ -5,3 +5,21 @@
  * Description: This is my test plugin
  * Version: 0.1.1
  */
+ 
+ namespace Will\MyLib;
+ 
+ abstract class Person {
+	    protected $name;
+	public function __construct(string $name) {
+		$this-> name = $name;	
+	}
+	public function sayHello($target) {
+		$this->obtainName();	
+	}
+	protected function getName() {
+		return 'Hello: '.$this->name;	
+	}
+ }
+ 
+ $person_1 = new Person('John');
+ var_dump($person_1->getName());
