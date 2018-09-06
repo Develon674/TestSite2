0fe26f0035052dd8a9f16f41fed865bf75df5c59
William Roberts <william@buzzgraphicdesign.co.uk>
2018-09-06 15:14:33 +0100

    Added bootstrap classes, plugin functions, composer

---
+++ classes/class-myplugin.php
@@ -0,0 +1,44 @@
+<?php 
+namespace Develon674\TestSite2;
+
+use WP_Term;
+use WP_Term_Query;
+
+class MyPlugin {
+    public function __construct() {
+        
+    }
+    
+    public function run() {
+        add_action('product_finder_body', function() {
+            echo $this->getOutput();
+        });
+    }
+    
+    /**
+     * @param WP_Term[] $categories A list of categories.
+    */
+    protected function printCatgories($categories) {
+        foreach ($categories as $category) {
+            echo $category->name;
+        }
+    }
+    
+    protected function getCatgories() {
+        $catQuery = new WP_Term_Query([
+            'taxonomy' => 'category', 
+                'hide_empty' => true,
+                     ]);
+
+        $terms = $catQuery->get_terms();
+        
+        return $terms;
+        
+    }
+    
+    protected function getOutput() {
+        $categories = $this->getCatgories();
+        return $this->printCatgories($categories);
+    }               
+                   
+}
--- plugin.php
+++ plugin.php
@@ -6,3 +6,19 @@
  * Version: 0.1.1
  * Text Domain: test-plugin 
  */
+
+require_once('vendor/autoload.php');
+
+use Dhii\Cache\MemoryMemoizer;
+use Dhii\Di\CachingContainer;
+use Develon674\TestSite2\MyPlugin;
+
+$cache = new MemoryMemoizer();
+$container = new CachingContainer([], $cache);
+
+$plugin = new MyPlugin();
+
+
+
+
+$plugin->run();
