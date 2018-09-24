123d85c29541c29ec7b532170b4362fbdf4ea700
William Roberts <william@buzzgraphicdesign.co.uk>
2018-09-24 17:10:21 +0100

    Modfication of template file
    Added image (w/lazyload) and content

--- templates/shortcode.php
+++ templates/shortcode.php
@@ -1,8 +1,18 @@
 <div class="myplugin-shortcode-container"></div>
 <script type="text/template" id="myplugin-list-template">
-    <% _.each(items, function(item) { %>
-      <div>
-        <h3><%= item.get('name') %></h3>
-      </div>
-    <% }); %>
+<% _.each(items, function(item) { %>
+<div>
+<% if (item.get('post_thumbnail_url')) { %>
+<div class="img<% if (!lazyload.check) { %> <%= lazyload.class %><% } %>" <% if (lazyload.check) { %>style="background-image:url(<% } else { %>data-src="<% } %><%= item.get('post_thumbnail_url') %><% if (lazyload.check) { %>);<% } %>"></div>
+<% } %>
+<% if (item.get('post_title') || item.get('post_content')) { %>
+<div class="content">
+<% if (item.get('post_title')) { %><h3 class="post-title"><%= item.get('post_title') %></h3><% } %>
+<% if (item.get('post_content')) { %>
+<%= item.get('post_content') %>
+<% } %>
+</div>
+<% } %>
+</div>
+<% }); %>
 </script>
